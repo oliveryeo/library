9ee44e16dc2d9cd865f391b56e8d850d49aaec0e
oliveryjy <yjyoliver@hotmail.com>
2023-08-16 22:04:57 +0800

    planned layout for the library

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 
+// Create example books
 let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 let harryPotter = new Book("Harry Potter", "J. K. Rowling", 989, false);
 let fantasticBeasts = new Book("Fantastic Beasts", "J. K. Rowling", 563, false);
