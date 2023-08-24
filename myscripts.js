4a69dcd6fec003a9ed0ce8c97ea80b6582577442
oliveryjy <yjyoliver@hotmail.com>
2023-08-24 17:24:55 +0800

    changed formatting for html and css

--- myscripts.js
+++ myscripts.js
@@ -13,75 +13,75 @@ initiateDisplay();
 
 // FUNCTIONS ------------------------------------------------------------
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(Book, index) {
-    const table = document.querySelector(".table");
-    const card = document.createElement("div");
-    const cardContent = document.createElement("div");
-    const bookTitle = document.createElement("div");
-    const bookAuthor = document.createElement("div");
-    const bookPages = document.createElement("div");
-    const bookRead = document.createElement("div");
-    const removeCard = document.createElement("div");
+  const table = document.querySelector(".table");
+  const card = document.createElement("div");
+  const cardContent = document.createElement("div");
+  const bookTitle = document.createElement("div");
+  const bookAuthor = document.createElement("div");
+  const bookPages = document.createElement("div");
+  const bookRead = document.createElement("div");
+  const removeCard = document.createElement("div");
 
-    // Set necessary classes and data-attributes to the elements
-    card.classList.add("card");
-    cardContent.classList.add("card-content");
-    bookTitle.classList.add("title");
-    bookAuthor.classList.add("author");
-    bookPages.classList.add("pages");
-    removeCard.classList.add("remove");
+  // Set necessary classes and data-attributes to the elements
+  card.classList.add("card");
+  cardContent.classList.add("card-content");
+  bookTitle.classList.add("title");
+  bookAuthor.classList.add("author");
+  bookPages.classList.add("pages");
+  removeCard.classList.add("remove");
 
-    // Add necessary content to each element
-    bookTitle.textContent = Book.title;
-    bookAuthor.textContent = Book.author;
-    bookPages.textContent = Book.pages + " Pages";
-    removeCard.textContent = "Remove";
+  // Add necessary content to each element
+  bookTitle.textContent = Book.title;
+  bookAuthor.textContent = Book.author;
+  bookPages.textContent = Book.pages + " Pages";
+  removeCard.textContent = "Remove";
 
-    // TODO -- If book is read, change background to green, if not read, change to red
-    // TODO -- Book read must be able to be clickable
-    if (Book.read === true) {
-        bookRead.textContent = "Read";
-        bookRead.classList.add("read");
+  // TODO -- If book is read, change background to green, if not read, change to red
+  // TODO -- Book read must be able to be clickable
+  if (Book.read === true) {
+    bookRead.textContent = "Read";
+    bookRead.classList.add("read");
+  } else {
+    bookRead.textContent = "Not read";
+    bookRead.classList.add("not-read");
+  }
+
+  // Add removecard function to removeCard button
+  removeCard.addEventListener("mouseup", () => {
+    delete myLibrary[index];
+    card.remove(); // somehow this is unique to the card created in this instance!
+  });
+
+  bookRead.addEventListener("mouseup", () => {
+    if (bookRead.classList.contains("read")) {
+      bookRead.classList.add("not-read");
+      bookRead.classList.remove("read");
+      bookRead.textContent = "Not read";
     } else {
-        bookRead.textContent = "Not read";
-        bookRead.classList.add("not-read");
+      bookRead.classList.add("read");
+      bookRead.classList.remove("not-read");
+      bookRead.textContent = "Read";
     }
+  });
 
-    // Add removecard function to removeCard button
-    removeCard.addEventListener("mouseup", () => {
-        delete myLibrary[index];
-        card.remove(); // somehow this is unique to the card created in this instance!
-    });
-
-    bookRead.addEventListener("mouseup", () => {
-        if (bookRead.classList.contains("read")) {
-            bookRead.classList.add("not-read");
-            bookRead.classList.remove("read");
-            bookRead.textContent = "Not read";
-        } else {
-            bookRead.classList.add("read");
-            bookRead.classList.remove("not-read");
-            bookRead.textContent = "Read";
-        }
-    })
-
-    // Append the elements as children in the correct order
-    let cardContents = [bookTitle, bookAuthor, bookPages, bookRead, removeCard];
-    table.appendChild(card);
-    card.appendChild(cardContent);
-    for (let i = 0; i < cardContents.length; i++) {
-        cardContent.appendChild(cardContents[i]);
-    }
+  // Append the elements as children in the correct order
+  let cardContents = [bookTitle, bookAuthor, bookPages, bookRead, removeCard];
+  table.appendChild(card);
+  card.appendChild(cardContent);
+  for (let i = 0; i < cardContents.length; i++) {
+    cardContent.appendChild(cardContents[i]);
+  }
 }
 
 function initiateDisplay() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        addBookToLibrary(myLibrary[i], i);
-    }
+  for (let i = 0; i < myLibrary.length; i++) {
+    addBookToLibrary(myLibrary[i], i);
+  }
 }
